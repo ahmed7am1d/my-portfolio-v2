9180462996a7ce8385d45a2858870e45840d9e40
ahmed <ahmed@backtech.cz>
2024-10-30 23:55:30 +0300

    feat: some layout stuff :)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="layout">
-    <NavBar />
+    <!-- Header/Navbar -->
+    <header>
+      <NavBar />
+    </header>
 
-    <slot />
+    <!-- Main -->
+    <main>
+      <slot />
+    </main>
+
+    <!-- Footer -->
+    <footer></footer>
   </div>
 </template>
 
 <style lang="scss">
-
 .layout {
   @apply w-full min-h-full h-full dark:bg-theme-dark bg-theme-light;
+
+  main {
+    @apply h-auto lg:w-4/5 mx-auto border-red-50 border-x-2;
+  }
 }
 </style>
