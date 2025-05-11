ae048aa41ad879cdff78ca7087dfa6a067694740
ahmed <ahmadhamidty@gmail.com>
2025-05-11 15:47:04 +0300

    fix(frontend): hydration mismatch

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -31,13 +31,15 @@ const { setLocale } = useI18n()
 
           <!-- Dark mode (just testing) -->
           <div class="col-span-5">
-            <h1 class="dark:text-white">Color mode: {{ colorMode.value }} (bad design I know 😁)</h1>
-            <select v-model="$colorMode.preference">
-              <option value="system">System</option>
-              <option value="light">Light</option>
-              <option value="dark">Dark</option>
-              <option value="sepia">Sepia</option>
-            </select>
+            <ClientOnly>
+              <h1 class="dark:text-white">Color mode: {{ colorMode.value }} (bad design I know 😁)</h1>
+              <select v-model="$colorMode.preference">
+                <option value="system">System</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+                <option value="sepia">Sepia</option>
+              </select>
+            </ClientOnly>
           </div>
 
           <!-- Locale (just testing) -->
